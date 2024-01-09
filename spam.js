a8133303013c03b61617cbc401636c923636ac57
alfin09 <156033917+alfin09@users.noreply.github.com>
2024-01-09 12:59:41 +0700

    Update spam.js

--- spam.js
+++ spam.js
@@ -1,29 +1,33 @@
-let token = "6872569198:AAGJmM52mqEwamJc06NjPxza0b-RzXFaXLw";
-let chatId = "6425729524";
-let photoUrl =
-  "https://th.bing.com/th/id/OIP.rE5rIe5-2-a8vXfSB7c2RwAAAA?rs=1&pid=ImgDetMain";
-
+let token = "YOUR_BOT_TOKEN";
+let chatId = "YOUR_CHAT_ID";
+let photoUrl = "YOUR_PHOTO_URL";
 let apiUrl = `https://api.telegram.org/bot${token}/sendPhoto?chat_id=${chatId}&photo=${photoUrl}`;
-let delay = 0.001;
+let delay = 1000; // Delay in milliseconds (1 second)
+
 function kirimGambar() {
   fetch(apiUrl, { method: "POST" })
     .then((response) => response.json())
     .then((data) => {
       console.log("Pesan berhasil dikirim:", data);
 
-      setTimeout(kirimGambar, delay);
+      if (data.ok === false) {
+        console.log("Response is ok: false. Pausing execution...");
+        // Pause execution for a specific time before resuming
+        setTimeout(kirimGambar, delay * 10); // Wait 10 times longer (10 seconds)
+      } else {
+        console.log("Response is ok: true. Resuming execution...");
+        setTimeout(kirimGambar, delay / 1000000);
+      }
     })
     .catch((error) => {
       console.error("Terjadi kesalahan:", error);
-
       setTimeout(kirimGambar, delay);
     });
 }
 
 function run() {
-  for (let i = 0; i < 1000; i++) {
+  for (let i = 0; i < 100; i++) {
     kirimGambar();
-    setTimeOut(run,0.0001);
   }
 }
 run();
